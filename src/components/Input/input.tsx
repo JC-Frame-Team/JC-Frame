6c2a9a25b502cce4db86e84aa132ff2eccb079af
shijiexiao <shijiexiao@tencent.com>
2020-06-23 11:44:42 +0800

    test Input push

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -23,7 +23,7 @@ export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size
 }
 
 /**
- * Input 输入框 通过鼠标或键盘输入内容，是最基础的表单域的包装。
+ * Input 输入框 通过鼠标或键盘输入内容， 是最基础的表单域的包装。
  * 
  * ~~~js
  * // 这样引用
