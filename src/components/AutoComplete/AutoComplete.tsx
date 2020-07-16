d09977fc006ef31c05db23d1099574c912667f35
jiejackhuang <41038972+jiejackhuang@users.noreply.github.com>
2020-07-16 12:04:08 +0800

    Update AutoComplete.tsx
    delete

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,4 +1,4 @@
-// 赵昌浩
+
 import React, { FC, useState, ChangeEvent, KeyboardEvent, ReactElement, useEffect, useRef } from 'react'
 import classNames from 'classnames'
 import Input, { InputProps } from '../Input/input'
